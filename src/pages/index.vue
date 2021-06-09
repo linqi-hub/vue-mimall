e95fdbe35d21d4af0eac29286589b5ca62871d97
linqi-hub <459467141@qq.com>
2021-06-09 11:29:29 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,100 @@
 <template>
-  <div>
-    <nav-header></nav-header>
-    <router-view></router-view>
-    <nav-footer></nav-footer>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item"><a href="javascript:;">手机 电话卡</a></li>
+            <li class="menu-item"><a href="javascript:;">电视 盒子</a></li>
+            <li class="menu-item"><a href="javascript:;">笔记本 平板</a></li>
+            <li class="menu-item"><a href="javascript:;">家电 插线板</a></li>
+            <li class="menu-item"><a href="javascript:;">出行 穿戴</a></li>
+            <li class="menu-item"><a href="javascript:;">智能 路由器</a></li>
+            <li class="menu-item"><a href="javascript:;">电源 配件</a></li>
+            <li class="menu-item"><a href="javascript:;">生活 箱包</a></li>
+          </ul>
+        </div>
+        <swiper v-bind:options="swiperOption">
+          <swiper-slide v-for="(item, index) in slideList" v-bind:key="index">
+            <a v-bind:href="'/#/product/' + item.id"
+              ><img v-bind:src="item.img"
+            /></a>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
+    <service-bar></service-bar>
   </div>
 </template>
 
 <script>
-// import NavHeader from "./../components/NavHeader";
-// import NavFooter from "./../components/NavFooter";
+import ServiceBar from "./../components/ServiceBar";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css"; //自动去nodemodule里面去找
 export default {
   name: "index",
   components: {
-    // NavHeader,
-    // NavFooter,
+    swiper,
+    swiperSlide,
+    ServiceBar,
+  },
+  data() {
+    return {
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: "cube",
+        cubeEffect: {
+          slideShadows: true,
+          shadow: true,
+          shadowOffset: 100,
+          shadowScale: 0.6,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg",
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg",
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg",
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.index {
+  .swiper-box {
+    .swiper-container {
+      height: 451px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
